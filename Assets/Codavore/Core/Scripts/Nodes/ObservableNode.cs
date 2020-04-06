a9e335e7252d9109bee2aaa3ba02c0fe7b89313a
Dan Violet Sagmiller <Game.Deb@Live.com>
2020-04-06 07:33:57 -0700

    Updated Observable Node/root to flat style of saving, while still maintaining pathing.  Observable Nodes have become much simpler. In Game has no reason to use lineage, so that was off loaded to a slower process, allowing the majority of the loading/saving to happen much easier. Saving is taking a small hit from this, but could be refined later.  The code is NOT RUNNABLE at this stage, but should be much lighter and easier to manage.

--- Assets/Codavore/Core/Scripts/Nodes/ObservableNode.cs
+++ Assets/Codavore/Core/Scripts/Nodes/ObservableNode.cs
@@ -16,92 +16,31 @@ namespace Codavore.Core
         Guid GetGuid();
 
         T GetValue<T>();
+
+        string GetPath();
         
         void SetValue(object value);
         
         void StartListening(Action listener);
         
         void StopListening(Action listener);
-        
-        IObservableNode GetParent();
 
         string GetName();
 
-        IObservableNode GetChild(string name);
-
-        IObservableNode GetChildFromPath(string path);
-
-        IEnumerable<IObservableNode> GetChildren();
-
-        void SaveToJson(JsonWriter writer);
-
-        void LoadFromJson(JsonTextReader reader, bool overwrite = false);
+        void Reset(string name, Guid guid, string path, object value);
     }
 
     public class ObservableNode : IObservableNode
     {
-        [JsonProperty]
-        private object Value;
-
-        [JsonIgnore]
         private Guid Guid;
+        private string Name;
+        private object Value;
+        private string Path;
+        private Action OnChange = () => { };
 
-        [JsonIgnore]
-        private Action OnValueChange = () => { };
-
-        [JsonIgnore]
-        private IObservableNode Parent = null;
-
-        [JsonProperty]
-        private string Name = "";
-
-        [JsonProperty]
-        private Dictionary<string, IObservableNode> Children
-            = new Dictionary<string, IObservableNode>();
-
-        public ObservableNode(string name, IObservableNode parent)
+        public Guid GetGuid()
         {
-            this.Name = name;
-            this.Parent = parent;
-            this.Guid = Guid.NewGuid();
-        }
-
-        public ObservableNode(string name, IObservableNode parent, Guid guid)
-        {
-            this.Name = name;
-            this.Parent = parent;
-            this.Guid = guid;
-        }
-
-        public T GetValue<T>()
-        {
-            return (T)this.Value;
-        }
-
-        public void SetValue(object value)
-        {
-            if (object.Equals(value, this.Value))
-            {
-                return;
-            }
-
-            this.Value = value;
-            this.OnValueChange();
-        }
-
-        public void StartListening(Action listener)
-        {
-            this.OnValueChange += listener;
-        }
-
-        public void StopListening(Action listener)
-        {
-            this.OnValueChange -= listener;
-        }
-
-        public IObservableNode GetParent()
-        {
-            return this.Parent;
+            return this.Guid;
         }
 
         public string GetName()
@@ -109,168 +48,64 @@ namespace Codavore.Core
             return this.Name;
         }
 
-        public IEnumerable<IObservableNode> GetChildren()
+        public string GetPath()
         {
-            foreach(var child in this.Children)
-            {
-                yield return child.Value;
-            }
+            return this.Path;
         }
 
-        public IObservableNode GetChild(string name)
+        public T GetValue<T>()
         {
-            IObservableNode child = null;
-            var childAlreadyExists = this.Children.ContainsKey(name);
-
-            if (!childAlreadyExists)
-            {
-                child = new ObservableNode(name, this);
-                this.Children.Add(name, child);
-            }
-            else
-            {
-                child = this.Children[name];
-            }
-
-            return child;
+            return (T)this.Value;
         }
 
-        public IObservableNode GetChildFromPath(string path)
+        public void Reset(string name, Guid guid, string path, object value)
         {
-            if (string.IsNullOrEmpty(path))
-            {
-                return GetChild(path);
-            }
-
-            var isNotPath = !path.Contains(CodavoreCoreConstants.PathSeparaterString);
-            if (isNotPath)
-            {
-                return GetChild(path);
-            }
-
-            var segments = path.Split(CodavoreCoreConstants.PathSeparaterCharArray);
-            IObservableNode node = this;
-            foreach(var segment in segments)
-            {
-                node = node.GetChild(segment);
-            }
-            
-            return node;
+            this.Name = name;
+            this.Guid = guid;
+            this.Path = path;
+            this.SetValue(value);
         }
 
-        public void SaveToJson(JsonWriter writer)
+        public void SetValue(object value)
+
+
         {
-            writer.WriteStartObject();
-            // name, .type, .value, .children (then lead to <name repeating)
-            if (this.Value != null)
-            {
-                writer.WritePropertyName("type");
-                writer.WriteValue(this.Value.GetType().FullName);
-
-                writer.WritePropertyName("value");
-                JsonSerializer.CreateDefault().Serialize(writer, this.Value);
-            }
-
-            writer.WritePropertyName("childCount");
-            writer.WriteValue(this.Children.Count);
-            if (this.Children.Count > 0)
-            {
-                writer.WritePropertyName("children");
-                writer.WriteStartArray();
-                foreach (var child in this.Children.Values)
-                {
-                    writer.WriteStartObject();
-                    writer.WritePropertyName("name");
-                    writer.WriteValue(child.GetName());
-                    writer.WritePropertyName("data");
-                    child.SaveToJson(writer);
-                }
-
-                writer.WriteEndArray();
-            }
-            writer.WriteEndObject();
-        }
-        private static JObject GetChild(
-            JObject jObj, 
-            string key)
-        {
-            var foundObj = (JObject)jObj
-                .Children()
-                .First((o) =>
-            {
-                var p = (JProperty)o.Parent;
-                return p.Name == "value";
-            }); 
-
-            return foundObj;
-        }
-        public void LoadFromJson(JsonTextReader reader, bool overwrite = false)
-        {
-            reader.Validate(JsonToken.StartObject, "'node' object not started correctly.");
-            reader.Read();
-
-            if (reader.TokenType == JsonToken.EndObject) return;
-
-            reader.Validate(JsonToken.PropertyName, read: false);
-            var propName = (string)reader.Value;
-            if (propName == "type")
-            {
-                reader.Read();
-                // type and value exist
-                var objectTypeName = (string)reader.Value;
-                var objectType = Type.GetType(objectTypeName);
-
-                reader.Validate(JsonToken.PropertyName, name: "value");
-                reader.Read(); // starts the value, so the next line can work
-                var value = JsonSerializer.CreateDefault().Deserialize(reader, objectType);
-                if (overwrite)
-                {
-                    this.SetValue(value);
-                }
-                reader.Read();
-            }
-
-            if (reader.TokenType == JsonToken.EndObject)
+            if (value == null && this.Value == null)
             {
                 return;
             }
 
-            reader.Validate(JsonToken.PropertyName, name: "childCount", read: false);
-            var childCount = reader.ReadAsInt32();
-            Debug.Log("C#: " + childCount);
+            if (value != null && this.Value == null)
+            {
+                this.Value = value;
+                this.OnChange();
+                return;
+            }
 
-            //if (reader.TokenType == JsonToken.EndObject)
-            if (reader.Validate(JsonToken.EndObject, errorOnFailure: false))
+            if (value == null && this.Value != null)
+            {
+                this.Value = null;
+                this.OnChange();
+                return;
+            }
+
+            if (this.Value.Equals(value))
             {
                 return;
             }
 
-            reader.Validate(JsonToken.PropertyName, name: "children", read: false);
-            reader.Validate(JsonToken.StartArray);
-            for (int i = 0; i < childCount; i++)
-            {
-                reader.Validate(JsonToken.StartObject);
-
-                reader.Validate(JsonToken.PropertyName, name: "name");
-                reader.Validate(JsonToken.String);
-                var childName = (string)reader.Value;
-
-                var child = this.GetChild(childName);
-                reader.Validate(JsonToken.PropertyName, name: "data");
-
-                child.LoadFromJson(reader, overwrite);
-
-                reader.Validate(JsonToken.EndObject);
-            }
-
-            reader.Validate(JsonToken.EndArray);
-            reader.Validate(JsonToken.EndObject);
-
+            this.Value = value;
+            this.OnChange();
         }
 
-        public Guid GetGuid()
+        public void StartListening(Action listener)
         {
-            throw new NotImplementedException();
+            this.OnChange += listener;
+        }
+
+        public void StopListening(Action listener)
+        {
+            this.OnChange -= listener;
         }
     }
 }
--- Assets/Codavore/Core/Scripts/Nodes/ObservableRoot.cs
+++ Assets/Codavore/Core/Scripts/Nodes/ObservableRoot.cs
@@ -19,143 +19,74 @@ namespace Codavore.Core
 
     public class ObservableRoot : IObservableRoot
     {
-        [JsonProperty]
-        private Dictionary<string, IObservableNode> RootNodes
-            = new Dictionary<string, IObservableNode>();
+        private Dictionary<Guid, IObservableNode> Nodes = new Dictionary<Guid, IObservableNode>();
 
-        private Action<string> AttemptingToLoadNode = (s) => { };
-
-        public IObservableNode GetNode(string path)
+        public IObservableNode GetNode(Guid guid)
         {
-            if (string.IsNullOrEmpty(path))
+            if (this.Nodes.ContainsKey(guid))
             {
-                return null;
+                return this.Nodes[guid];
             }
 
-            if (path.Contains(CodavoreCoreConstants.PathSeparaterString))
+            return null;
+        }
+
+        public List<string> GetPaths()
+        {
+            var paths = new List<string>();
+            foreach(var pair in this.Nodes)
             {
-                var pathElements = path.Split(CodavoreCoreConstants.PathSeparaterCharArray);
-                var node = GetRootNode(pathElements[0]);
-                for(int i = 1; i < pathElements.Length; i++)
+                var path = pair.Value.GetPath();
+                if (paths.Contains(path))
                 {
-                    node = node.GetChild(pathElements[i]);
+                    continue;
                 }
 
-                return node;
+                paths.Add(path);
+            }
+
+            return paths;
+        }
+
+        public List<IObservableNode> GetNodes(string path, bool withLineage = false)
+        {
+            var nodes = new List<IObservableNode>();
+            if (withLineage)
+            {
+                foreach (var pair in this.Nodes)
+                {
+                    if (pair.Value.GetPath().StartsWith(path))
+                    {
+                        nodes.Add(pair.Value);
+                    }
+                }
             }
             else
             {
-                return GetRootNode(path);
-            }
-        }
-
-        private IObservableNode GetRootNode(string name)
-        {
-            if (this.RootNodes.ContainsKey(name))
-            {
-                return this.RootNodes[name];
+                foreach (var pair in this.Nodes)
+                {
+                    if (pair.Value.GetPath() == path)
+                    {
+                        nodes.Add(pair.Value);
+                    }
+                }
             }
 
-            this.AttemptingToLoadNode(name);
-
-            if (this.RootNodes.ContainsKey(name))
-            {
-                return this.RootNodes[name];
-            }
-
-            IObservableNode rootNode = new ObservableNode(name, null);
-            this.RootNodes.Add(name, rootNode);
-            return rootNode;
+            return nodes;
         }
 
-        public IEnumerable<IObservableNode> GetRootNodes()
+        public string SaveLineage(string path)
         {
-            foreach (var node in this.RootNodes.Values)
-            {
-                yield return node;
-            }
+            var nodes = GetNodes(path, withLineage: true);
+            // begin jsonWriter
+            // Start Array
+            // Save All
+            // return string
         }
 
-        public void StartListeningToLoadRequest(Action<string> loadHandler)
+        public string LoadLineage(string json)
         {
-            this.AttemptingToLoadNode += loadHandler;
-        }
 
-        public void StopListeningToLoadRequest(Action<string> loadHandler)
-        {
-            this.AttemptingToLoadNode -= loadHandler;
-        }
-
-        public bool RootNodeExists(string name)
-        {
-            return this.RootNodes.ContainsKey(name);
-        }
-        public string SaveRootNode(string name)
-        {
-            var node = GetRootNode(name);
-            StringBuilder sb = new StringBuilder();
-            StringWriter sw = new StringWriter(sb);
-            using (JsonWriter writer = new JsonTextWriter(sw))
-            {
-                writer.Formatting = Formatting.Indented;
-                writer.WriteStartObject();
-                writer.WritePropertyName("version");
-                writer.WriteValue("1");
-                writer.WritePropertyName("name");
-                writer.WriteValue(name);
-                writer.WritePropertyName("node");
-                node.SaveToJson(writer);
-                writer.WriteEndObject();
-            }
-            return sb.ToString();
-        }
-
-
-        //        public string SaveRootNode(string name)
-        //        {
-        //            var node = GetRootNode(name);
-        //            StringBuilder sb = new StringBuilder();
-        //            StringWriter sw = new StringWriter(sb);
-
-        //            using (JsonWriter writer = new JsonTextWriter(sw))
-        //            {
-        //                writer.Formatting = Formatting.Indented;
-        //                writer.WriteStartObject();
-        //                writer.WritePropertyName("version");
-        //                writer.WriteValue("1");
-        //                writer.WritePropertyName("name");
-        //                writer.WriteValue(name);
-        //                writer.WritePropertyName("node");
-        //                node.SaveToJson(writer);
-        //                writer.WriteEndObject();
-        //            }
-
-        //            return sb.ToString();
-        //        }
-
-        //#warning //TODO: RootNode needs an optional field to Overwrite existing fields and data, or to just add new items.  (I.e. reset level vs maintaining data.)
-        //        // All Events should be retained.
-        //        public void LoadRootNode(string json, bool overwrite = true)
-        //        {
-        //            // node, .type, .value, .children (then lead to <name repeating)
-        //            var reader = new JsonTextReader(new StringReader(json));
-        //            reader.Validate(JsonToken.StartObject, "Missing starting '{'");
-        //            reader.Validate(JsonToken.PropertyName, "First property must be 'version'", "version");
-        //            reader.Validate(JsonToken.String, "Improper JSON version.", "1");
-        //            reader.Validate(JsonToken.PropertyName, "'name' should be the second property.", "name");
-
-        //            reader.Validate(JsonToken.String, "'name' property should have had a string value.");
-        //            var name = (string)reader.Value;
-
-        //            reader.Validate(JsonToken.PropertyName, "'node' property should have been the third.", "node");
-        //            this.GetNode(name).LoadFromJson(reader, overwrite);
-
-        //            reader.Validate(JsonToken.EndObject, "Improper closing on node");
-        //        }
-
-        public void DeleteRootNode(string name)
-        {
-            this.RootNodes.Remove(name);
         }
     }
 }
